f2449b4766603de5f63624543e42ffb24ef386dd
aki <martin_ylx@outlook.com>
2023-12-01 16:59:10 +0800

    完善Book类

---
+++ Book.cpp
@@ -0,0 +1,391 @@
+#include "Book.h"
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+// todo: Detail & Tag
+
+Detail::Detail() {
+    name = "";
+    isbn_issn = "";
+    author = "";
+    publish = "";
+    publish_time = "";
+    price = "";
+    time = 0;
+}
+
+#define ull unsigned long long
+Detail::Detail(string nam,string isbn,string auth,string pub,string pub_time,string pri,ull tim) {
+    name = nam;
+    isbn_issn = isbn;
+    author = auth;
+    publish = pub;
+    publish_time = pub_time;
+    price = pri;
+    time = tim;
+}
+#undef ull
+
+void Detail::insert_tag(Tag *p,string s) {
+    Tag *temp = p;
+    while (temp->next == nullptr) {
+        temp = temp->next;
+    }
+    Tag *next = (Tag *)malloc(sizeof(Tag));
+    next->name = s;
+    next->next = nullptr;
+    temp->next = next;
+    next->pre = temp;
+    p->num ++;
+}
+
+void Detail::delete_tag(Tag *p,string s) {
+    Tag *temp = p->next;
+    Tag *pre = p;
+    while (temp->name != s) {
+        pre = temp;
+        temp = temp->next;
+    }
+    pre->next = temp->next;
+    free(temp);
+    p->num --;
+}
+
+void Detail::add_time(int x) {
+    time += x;
+}
+
+// todo: Book
+
+Book::Book() {
+    this->next = nullptr;
+}
+
+void Book::print() {
+    printf("书籍信息：\n");
+    printf("名称：\n");
+    cout << detail.name << endl;
+    printf("ISBN/ISSN：\n");
+    cout << detail.isbn_issn << endl;
+    printf("作者：\n");
+    cout << detail.author << endl;
+    printf("出版社：\n");
+    cout << detail.publish << endl;
+    printf("出版时间：\n");
+    cout << detail.publish_time << endl;
+    printf("价格：\n");
+    cout << detail.price << endl;
+    printf("借阅次数：%llu\n",detail.time);
+}
+
+void Book::insert(Detail in) {
+    detail.name = in.name;
+    detail.isbn_issn = in.isbn_issn;
+    detail.author = in.author;
+    detail.publish = in.publish;
+    detail.publish_time = in.publish_time;
+    detail.price = in.price;
+}
+
+bool Book::check(string isbn) {
+    if (this->detail.isbn_issn == isbn) return true;
+    else return false;
+}
+
+void insert_book(Book *p,Detail in) {
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    Book *next = (Book *)malloc(sizeof(Book));
+    next->insert(in);
+    next->next = nullptr;
+    temp->next = next;
+    next->pre = temp;
+    p->num ++;
+}
+
+void delete_book(Book *p,string isbn) {
+    Book *temp = p;
+    Book *pre;
+    while (!temp->check(isbn)) {
+        pre = temp;
+        temp = temp->next;
+    }
+    pre->next = temp->next;
+    free(temp);
+    p->num --;
+}
+
+void search_name(Book *p,Book *now) {
+    string name;
+    printf("输入你要查询的书籍名称：");
+    cin >> name;
+    Book *temp = p;
+    while (temp->detail.name != name) {
+        temp = temp->next;
+    }
+    insert_book(now,temp->detail);
+}
+
+void search_isbn(Book *p,Book *now) {
+    string isbn;
+    printf("输入你要查询的ISBN/ISSN：");
+    cin >> isbn;
+    Book *temp = p;
+    while (temp->detail.isbn_issn != isbn) {
+        temp = temp->next;
+    }
+    insert_book(now,temp->detail);
+}
+
+void search_author(Book *p,Book *now) {
+    string author;
+    printf("输入你要查询的作者：");
+    cin >> author;
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        if (temp->detail.author == author) {
+            insert_book(now,temp->detail);
+        }
+        temp = temp->next;
+    }
+}
+
+void search_publish(Book *p,Book *now) {
+    string publish;
+    printf("输入你要查询的出版社：");
+    cin >> publish;
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        if (temp->detail.publish == publish) {
+            insert_book(now,temp->detail);
+        }
+        temp = temp->next;
+    }
+}
+
+void search_publish_time(Book *p,Book *now) {  //todo：可添加模糊查询 年份
+    string publish_time;
+    printf("输入你要查询的出版时间：");
+    cin >> publish_time;
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        if (temp->detail.publish_time == publish_time) {
+            insert_book(now,temp->detail);
+        }
+        temp = temp->next;
+    }
+}
+
+void search_price(Book *p,Book *now) {
+    string price_low, price_high;
+    printf("输入你要查询的价格范围：\n最低：");
+    cin >> price_low;
+    printf("最高：");
+    cin >> price_high;
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        if (temp->detail.price >= price_low && temp->detail.price <= price_high) {
+            insert_book(now,temp->detail);
+        }
+        temp = temp->next;
+    }
+}
+
+void search_tag(Book *p,Book *now) {
+    string tag;
+    printf("输入你要查询的标签：");
+    cin >> tag;
+    Book *temp = p;
+    while (temp->next != nullptr) {
+        Tag *head = temp->detail.head;
+        while (head->next != nullptr) {
+            if (head->name == tag) {
+                insert_book(now,temp->detail);
+                break;
+            }
+            head = head->next;
+        }
+        temp = temp->next;
+    }
+}
+
+void change_name(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string name;
+        printf("请输入修改后的名称：");
+        cin >> name;
+        temp->detail.name = name;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_isbn(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string isbn;
+        printf("请输入修改后的ISBN/ISSN：");
+        cin >> isbn;
+        temp->detail.isbn_issn = isbn;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_author(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string author;
+        printf("请输入修改后的作者：");
+        cin >> author;
+        temp->detail.author = author;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_publish(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string publish;
+        printf("请输入修改后的出版社：");
+        cin >> publish;
+        temp->detail.publish = publish;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_publish_time(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string publish_time;
+        printf("请输入修改后的出版时间：");
+        cin >> publish_time;
+        temp->detail.publish = publish_time;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_price(Book *now) {
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        string price;
+        printf("请输入修改后的价格：");
+        cin >> price;
+        temp->detail.price = price;
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+void change_book(Book *p) {
+    printf("1.修改名称\n");
+    printf("2.修改ISBN/ISSN\n");
+    printf("3.修改作者\n");
+    printf("4.修改出版社\n");
+    printf("5.修改出版时间\n");
+    printf("6.修改价格\n");
+    printf("请选择你的操作：\n");
+    Book *now; //修改书籍头指针
+    int opt;
+    scanf("%d",&opt);
+    switch (opt) {
+        case 1:
+            search_name(p,now);
+            change_name(now);
+            break;
+        case 2:
+            search_isbn(p,now);
+            change_isbn(now);
+            break;
+        case 3:
+            search_author(p,now);
+            change_author(now);
+            break;
+        case 4:
+            search_publish(p,now);
+            change_publish(now);
+            break;
+        case 5:
+            search_publish_time(p,now);
+            change_publish_time(now);
+            break;
+        case 6:
+            search_price(p,now);
+            change_price(now);
+            break;
+        default:
+            printf("请重新选择\n");
+            change_book(p);
+    }
+}
+
+void search_book(Book *p) {
+    printf("1.名称\n");
+    printf("2.ISBN/ISSN\n");
+    printf("3.作者\n");
+    printf("4.出版社\n");
+    printf("5.出版时间\n");
+    printf("6.价格区间\n");
+    printf("请选择你的操作：\n");
+    Book *now; //修改书籍头指针
+    int opt;
+    scanf("%d",&opt);
+    switch (opt) {
+        case 1:
+            search_name(p,now);
+            break;
+        case 2:
+            search_isbn(p,now);
+            break;
+        case 3:
+            search_author(p,now);
+            break;
+        case 4:
+            search_publish(p,now);
+            break;
+        case 5:
+            search_publish_time(p,now);
+            break;
+        case 6:
+            search_price(p,now);
+            break;
+        default:
+            printf("请重新选择\n");
+            search_book(p);
+    }
+    Book *temp = now;
+    Book *pre;
+    while (temp->next != nullptr) {
+        temp->print();
+        pre = temp;
+        temp = temp->next;
+        free(pre);
+    }
+}
+
+
--- Book.h
+++ Book.h
@@ -4,37 +4,70 @@
 using namespace std;
 
 struct  Tag {
+    int num;
     string name;
+    Tag *pre;
     Tag *next;
-
-    void add(string s);
-    void sub(string s);
-};
+}*sum;
 
 #define ull unsigned long long
 struct Detail {
-    string name;
-    ull isbn_issn;
-    string author;
-    string publish;
-    ull publish_time;
-    double price;
-    ull time;
-    Tag *head = nullptr;
+    string name;   //书本名称
+    string isbn_issn;   //isbn号 issn号
+    string author;   //作者
+    string publish;   //出版社
+    string publish_time;   //出版时间
+    string price;   //价格
+    ull time;  //借阅次数
+    Tag *head = (Tag *) malloc(sizeof(Tag));
 
-    Detail(string nam,ull isbn,string auth,string pub,ull pub_time,double pri,ull tim);
-    void change_tag(string s); // 0 add 1 sub
-    void add_time();
-    
+    Detail(); //初始化
+    Detail(string nam,string isbn,string auth,string pub,string pub_time,string pri,ull tim); //插入内容
+    void insert_tag(Tag *p,string s); //增加tag
+    void delete_tag(Tag *p,string s); //删除tag
+    void add_time(int x);   //修改借阅次数
 };
+#undef ull
 
 class Book {
 private:
+    int num;
     Detail detail;
-    Book *next;
 public:
-    Book();
-    void add();
-    void sub();
-    void change();
-};
+    Book *pre;
+    Book *next;
+
+    Book(); //初始化
+    void print(); //输出全部信息
+    void insert(Detail in); //插入Detail
+    bool check(string isbn); //验证isbn issn
+    //构建
+    Book *head = (Book *)malloc(sizeof(Book)); //总目录 头指针
+    friend void change_book(Book *p); //修改book
+    friend void insert_book(Book *p,Detail in); //增加book
+    friend void delete_book(Book *p,string isbn); //减少book
+    //查询
+    friend void search_book(Book *p); //寻找book
+    friend void search_name(Book *p,Book *now);
+    friend void search_isbn(Book *p,Book *now);
+    friend void search_author(Book *p,Book *now);
+    friend void search_publish(Book *p,Book *now);
+    friend void search_publish_time(Book *p,Book *now);
+    friend void search_price(Book *p,Book *now);
+    friend void search_tag(Book *p,Book *now);
+    //修改
+    friend void change_name(Book *now);
+    friend void change_isbn(Book *now);
+    friend void change_author(Book *now);
+    friend void change_publish(Book *now);
+    friend void change_publish_time(Book *now);
+    friend void change_price(Book *now);
+    //排序
+
+
+
+};//todo:返回上级目录
+
+
+
+// todo: 字典序排序 借阅次数前10 出版时间前10 tag数目前10
